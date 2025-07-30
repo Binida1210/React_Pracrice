7c7bd237bb7fca55a449a0ddba9ebd9b6abbde06
Tran Xuan Binh <114062608+Binida1210@users.noreply.github.com>
2025-07-30 20:39:57 +0900

    Add RandomColor

---
+++ React_Projects_Based_Learning/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Accordian from "./Components/Accordian";
+import RandomColor from "./Components/RandomColor";
+
+function App() {
+  return (
+    <>
+      {/* <Accordian /> */}
+      <RandomColor />
+    </>
+  );
+}
+
+export default App;
